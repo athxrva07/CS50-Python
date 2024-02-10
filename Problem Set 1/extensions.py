84ca43acf1d07ef2eb635d385ca60049cf9d9e3a
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-02-10 23:14:30 +0000

    Add files via upload

---
+++ Problem Set 1/extensions.py
@@ -0,0 +1,19 @@
+name = input("Filename : ").lower().strip()
+
+name = '.' + name.split('.')[-1]
+
+match name:
+    case ".gif":
+        print("image/gif")
+    case ".jpg" | ".jpeg":
+        print("image/jpeg")
+    case ".png":
+        print("image/png")
+    case ".pdf":
+        print("application/pdf")
+    case ".txt":
+        print("text/plain")
+    case ".zip":
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
