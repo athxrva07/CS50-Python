6c65cf883ea478504f1074e52c8be4631e8261d4
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-02-11 22:05:31 +0000

    Add files via upload

---
+++ Problem Set 1/interpreter.py
@@ -0,0 +1,19 @@
+expression = input("Expression: ")
+
+x,y,z = expression.split(" ")
+
+x = int(x)
+z = int(z)
+
+match y:
+    case "+":
+        print(float(x+z))
+    case "-":
+        print(float(x-z))
+    case "*":
+        print(float(x*z))
+    case "/":
+        print(float(x/z))
+        
+
+
